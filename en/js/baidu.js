d93f6e6d896ec920be6ffae6f9834180cbf10cdd
XINLAN <kyowxf2003@163.com>
2024-05-06 15:04:42 +0800

    333

---
+++ en/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bc372f4b750d8887ec9fa8f6017b6007";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
